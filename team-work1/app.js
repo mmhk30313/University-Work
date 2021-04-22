4ef2bb53c20a7ba6b837321c8287d0955a87cbca
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2021-04-22 10:20:45 +0600

    Transition Added

--- team-work1/app.js
+++ team-work1/app.js
@@ -1,8 +1,5 @@
 const main = document.querySelector('#main');
-// main.innerHTML = `<a href="#" class='card bg-light rounded text-decoration-none'>
-// <p style="color: white;" class="text-center">Device 1</p>
-// </a>`;
-console.log(main)
+// console.log(main)
 function getData(){
     
     fetch('https://my-all-works-server.herokuapp.com/')
@@ -23,18 +20,18 @@ function showData(data){
     data.forEach(d => {
         const div = document.createElement('div')
         div.className = 'col-lg-4 col-md-6 col-xs-6 my-2';
-        div.innerHTML = `<a href="#${d.name}" class='card hover bg-white text-center p-2 bg-light rounded text-decoration-none text-dark shadow'>
+        div.innerHTML = `<a  href="#${d.name}" class='card hover bg-white text-center p-2 bg-light rounded text-decoration-none text-dark shadow'>
             <h4>${d.name}</h4>
             <img style="height: 200px" class="img-fluid w-100 mx-auto" src="${d.img}" alt=""/>
         </a>`;
         row.appendChild(div);
     });
-    console.log(row)
+    // console.log(row)
     return row;
 }
 
 function showDetails(devices){
-    console.log(devices)
+    // console.log(devices)
     const article = document.createElement('article');
     article.classList.add("py-4");
     const h2 = document.createElement('h2');
